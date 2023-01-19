ce8198ae4bc38429e08ff961577c30d4cfc5a23c
Erisvan Barboza <erisvanbarboza@gmail.com>
2023-01-19 19:45:49 -0300

    Atualizações gerais e js

--- Site/src/js/script.js
+++ Site/src/js/script.js
@@ -1,1 +1,28 @@
-// codigo js 
+const pessoas = [];
+const cadastro = document.querySelector(".cadastro");
+
+const usuarios = {
+    nome: document.querySelector("#nome").value,
+    sobrenome: document.querySelector("#sobrenome").value,
+    email: document.querySelector("#email").value,
+}
+pessoas.push(usuarios);
+
+let cadastrar = () => {
+
+    if (usuarios.value === ""){
+        alert ("escreva no campo");
+    }else {
+        pessoas.push(usuarios)
+        percorrer();
+    }
+};
+
+let percorrer = () => {
+    for (let i = 0; i < pessoas.length; i++){
+        let pessoa = document.createElement("li");
+        pessoa.innerHTML = pessoas[i];
+        cadastro.appendChild(pessoa);
+
+    }
+};
