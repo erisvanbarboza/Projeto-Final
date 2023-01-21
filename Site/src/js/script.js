4f5ae1728ed033ef2b5510c8359e6b2de9d3d17a
Erisvan Barboza <erisvanbarboza@gmail.com>
2023-01-20 23:26:41 -0300

    Adicionando Menu dinâmico

--- Site/src/js/script.js
+++ Site/src/js/script.js
@@ -1,28 +1,55 @@
-const pessoas = [];
-const cadastro = document.querySelector(".cadastro");
+// const pessoas = [];
+// const cadastro = document.querySelector(".cadastro");
 
-const usuarios = {
-    nome: document.querySelector("#nome").value,
-    sobrenome: document.querySelector("#sobrenome").value,
-    email: document.querySelector("#email").value,
-}
-pessoas.push(usuarios);
+// const nome = document.querySelector("#nome");
+// const sobrenome = document.querySelector("#sobrenome");
+// const email = document.querySelector("#email");
 
-let cadastrar = () => {
+const menu = document.querySelector("#menu");
 
-    if (usuarios.value === ""){
-        alert ("escreva no campo");
-    }else {
-        pessoas.push(usuarios)
-        percorrer();
+const menus = [
+    {
+        nome: "Home",
+        link: "./index.html",
+    }, 
+    {
+        nome: "Sobre",
+        link: "./sobre.html",
+    }, 
+    {
+        nome: "Cadastro",
+        link: "./cadastro.html",
     }
-};
+];
 
-let percorrer = () => {
-    for (let i = 0; i < pessoas.length; i++){
-        let pessoa = document.createElement("li");
-        pessoa.innerHTML = pessoas[i];
-        cadastro.appendChild(pessoa);
+menus.forEach((item) => {
 
-    }
-};
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+
+    li.classList.add("lista");
+    menu.appendChild(li);
+    link.innerText = item.nome;
+    link.href= item.link;
+    li.appendChild(link);
+}) 
+// pessoas.push(usuarios);
+
+// let cadastrar = () => {
+
+//     if (usuarios.value === ""){
+//         alert ("escreva no campo");
+//     }else {
+//         pessoas.push(usuarios)
+//         percorrer();
+//     }
+// };
+
+// let percorrer = () => {
+//     for (let i = 0; i < pessoas.length; i++){
+//         let pessoa = document.createElement("li");
+//         pessoa.innerHTML = pessoas[i];
+//         cadastro.appendChild(pessoa);
+
+//     }
+// };
